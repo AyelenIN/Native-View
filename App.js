3c70bc1a62b278f2dc688ad8ed3a22a7360cb99c
AyelenIN <ivanainsa2002@gmail.com>
2022-11-18 10:14:37 -0300

    subir

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <WebView source={{ uri: 'http://192.168.216.167:3000' }} />
+
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
